1f705088e0adc8520246f23c53e9e34f348e4c67
sdemecilio <stacey.demecilio@live.com>
2016-01-12 10:52:12 -0800

    First version of site.

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang = "en">
+    
+    <head>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Honey Bee Data Collection</title>
+        <meta charset = "UTF-8">
+        
+        <!--Bootstrap -->
+        <link href = "css/bootstrap.min.css" rel = "stylesheet">
+    </head>
+    
+    <body>
+        <h1>Honey Bee Data Collection Form</h1>
+        <!--
+            Need:
+                - Hive name
+                - Observation date
+                - observation duration
+                - mite count
+            -->
+        
+        <form id = "dataCollection" action = "record.php" method = "POST">
+            <p>
+                Hive Name:
+                    <input type = "text" id = "hiveName" name = "hiveName">
+            </p>
+            <p>
+                Date of Collection:
+                    <input type = "date" id = "dateCollection" name = "dateCollection" required>
+            </p>
+            <p>
+                Observation period:
+                    <input type = "number" id = "observationPeriod" name = "observationPeriod" min = "1" max = "500" required>
+            </p>
+            <p>
+                Mite count:
+                    <input type = "number" id = "miteCount" name = "miteCount" min = "0" max = "1000" required>
+            </p>
+            
+            <!-- Submit button -->
+            <p>
+                <input type = "submit" name = "btnSubmit" value = "Submit Data">
+            </p>
+        </form>
+    </body>
+    
+</html>
---
+++ record.php
@@ -0,0 +1,93 @@
+<?php
+
+    /*
+     * Stacey Demecilio
+     * Process data for index.php form
+     *
+     * Array
+(
+    [hiveName] => Honey123
+    [dateCollection] => 2014-01-06
+    [observationPeriod] => 5
+    [miteCount] => 6
+    [btnSubmit] => Submit Data
+)
+     */
+    
+    // turn on error reporting
+    ini_set('display_errors', 1);
+    error_reporting(E_ALL);
+?>
+
+<!DOCTYPE html>
+    <html>
+        <head>
+            <title>Data Recording</title>
+        </head>
+        
+        <body>
+            
+            <?php
+                
+                
+                // create a boolean flag
+                $isValid = true;
+                
+                // check for hive name
+                if(!empty ($_POST['hiveName']))
+                {
+                    $hiveName = $_POST['hiveName'];
+                }
+                else
+                {
+                   $hiveName = "No hive name entered";
+                }
+                
+                // check for collection date
+                if(!empty($_POST['dateCollection']))
+                {
+                    $collectionDate = $_POST['dateCollection'];
+                }
+                
+                // check for observation period
+                if(!empty($_POST['observationPeriod']))
+                {
+                    $observationPeriod = $_POST['observationPeriod'];
+                }
+                
+                //check number of mite count
+                if(!empty($_POST['miteCount']))
+                {
+                    $miteCount = $_POST['miteCount'];
+                }
+                else
+                {
+                    $miteCount = 0;
+                }
+                
+                // display summary
+                if($isValid)
+                {
+                    // read from database
+                require'../../../honeyDB.php';
+                
+                    // escape the data (rejects sql injection)
+                    $hiveName = mysqli_real_escape_string($cnxn, $hiveName);
+                    
+                    $sql = "INSERT INTO bee_observations VALUES(NULL, '$hiveName', '$collectionDate', '$observationPeriod', '$miteCount', NOW());";
+                    echo $sql; // <--- shows what the sql statemet is 
+                    
+                    print "<p></p>";
+                    print "Hive Name: " . $hiveName . "<br>";
+                    print "Collection date: " . $collectionDate . "<br>";
+                    print "Observation Period: " . $observationPeriod . "<br>";
+                    print "Mite count: " . $miteCount . "<br>";
+                }
+            ?>
+        
+        <p><a href = "index.php"><h3>Back to data input.</h3></a></p>
+            
+        </body>
+    </html>
+    
+    
