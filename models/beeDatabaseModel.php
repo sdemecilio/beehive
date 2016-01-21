af1385deb7254b7bcec1a50dfd8441ff1c6727af
sdemecilio <stacey.demecilio@live.com>
2016-01-21 09:50:11 -0800

    First version of model.

---
+++ models/beeDatabaseModel.php
@@ -0,0 +1,22 @@
+<?php
+    
+    class beeDatabaseModel
+    {
+        var $db;
+        
+        public function beeDatabaseModel construct(PDO $db)
+        {
+            $this->db = $db;
+        }
+        
+        public function getAllObservations()
+        {
+            $sql = "SELECT hive_id, collection_id, sample_period, num_mites FROM bee_observations";
+            
+            return $this->db->query($query);
+            
+        }
+        
+    }
+    
+?>
