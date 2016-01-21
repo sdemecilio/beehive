936a87985c6607de58b1ba78ac05f55284778a75
sdemecilio <stacey.demecilio@live.com>
2016-01-21 10:09:35 -0800

    controller for project.

---
+++ admin.php
@@ -0,0 +1,32 @@
+<?php
+
+    $username = "stacey_beeAdmin";
+    $password = "bee123";
+    $hostname = "localhost";
+            
+    try
+    {
+        $db = new PDO ("mysql:host=$hostname; dbname = stacey_bee_database", $username, $password);
+        echo 'Connected!';
+    }
+    catch (PDOException $e)
+    {
+        echo $e->getMessage();
+    }
+    
+    // make model available
+    include 'models/beeDatabaseModel.php';
+    
+    // create instance of data model
+    $model = new beeDatabaseModel($db);
+    
+    // call getAllObservations function
+    $beeList = $model->getAllObservations();
+    
+    // show view
+    include 'views/beeList.php';
+    
+    // close database conncection
+    $db = null;
+    
+?>
