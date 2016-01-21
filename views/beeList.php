687ffa20a4fdea9447c17e705a970e3de7d4a9b2
sdemecilio <stacey.demecilio@live.com>
2016-01-21 11:00:49 -0800

    Initial version of the views file.

---
+++ views/beeList.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang = "en">
+    <head>
+        <title>Beehive Prototype</title>
+        <meta charset = "UTF-8">
+        <meta name = "author" content = "Beehive">
+    </head>
+    
+    <body>
+        <table>
+            <?php
+            foreach ($beeList as $row)
+            {
+                echo '<tr>';
+                echo '<td>', $row['hive_id'], '</td>';
+                echo '<td>', $row['collection_date'], '</td>';
+                echo '<td>', $row['sample_period'], '</td>';
+                echo '<td>', $row['num_mites'], '</td>';
+                echo '</tr>';
+            }
+            ?>
+        </table>
+    </body>
+</html>
